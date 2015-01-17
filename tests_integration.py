27249c06a9b55a1474342ae66c5225c7976ebca7
mattjhayes <matthew_john_hayes@hotmail.com>
2015-01-18 09:03:07 +1300

    update from mattjhayes

--- tests_integration.py
+++ tests_integration.py
@@ -3,7 +3,6 @@ Nmeta Integration Tests
 .
 To run, type in nosetests in the nmeta directory
 """
-
 import tc_policy
 from ryu.ofproto import ether
 from ryu.lib.packet import ethernet, arp, packet, ipv4, tcp
@@ -13,37 +12,49 @@ from ryu.lib.packet import ethernet, arp, packet, ipv4, tcp
 tc = tc_policy.TrafficClassificationPolicy \
                     ("DEBUG","DEBUG","DEBUG","DEBUG","DEBUG")
 #*** Test values for policy_conditions:
-conditions_any_openflow = {'match_type': 'any', 
+conditions_any_openflow = {'match_type': 'any',
                              'tcp_src': 6633, 'tcp_dst': 6633}
-conditions_all_openflow = {'match_type': 'all', 
+conditions_all_openflow = {'match_type': 'all',
                              'tcp_src': 6633, 'tcp_dst': 6633}
-conditions_any_mac = {'match_type': 'any', 'eth_src': '08:60:6e:7f:74:e7', 
+conditions_any_mac = {'match_type': 'any', 'eth_src': '08:60:6e:7f:74:e7',
                          'eth_dst': '08:60:6e:7f:74:e8'}
-conditions_all_mac = {'match_type': 'all', 'eth_src': '08:60:6e:7f:74:e7', 
+conditions_all_mac = {'match_type': 'all', 'eth_src': '08:60:6e:7f:74:e7',
                          'eth_dst': '08:60:6e:7f:74:e8'}
-conditions_any_ip = {'match_type': 'any', 'ip_dst': '192.168.57.12', 
+conditions_any_ip = {'match_type': 'any', 'ip_dst': '192.168.57.12',
                          'ip_src': '192.168.56.32'}
 conditions_any_ssh = {'match_type': 'any', 'tcp_src': 22, 'tcp_dst': 22}
-conditions_nested = {'conditions 2': {'match_type': 'any', 
+conditions_nested = {'conditions 2': {'match_type': 'any',
                 'identity_lldp_systemname_re': '.*\\.audit\\.example\\.com'}, 
                 'conditions 1': {'match_type': 'any', 'tcp_src': 22, 
                 'tcp_dst': 22}, 'match_type': 'all'}
+results_dict_no_match = {'actions': False, 'match': False,
+                     'continue_to_inspect': False}
+results_dict_match = {'actions': False, 'match': True,
+                     'continue_to_inspect': False}
 
 #*** Check Match Validity Tests:
 def test_check_conditions():
     #*** Test Packets:
     pkt_arp = build_packet_ARP()
     pkt_tcp_22 = build_packet_tcp_22()
-    tc._check_conditions(pkt_arp, conditions_any_openflow) == 0
-    tc._check_conditions(pkt_arp, conditions_all_openflow) == 0
-    tc._check_conditions(pkt_arp, conditions_any_mac) == 1
-    tc._check_conditions(pkt_arp, conditions_all_mac) == 0
-    tc._check_conditions(pkt_arp, conditions_nested) == 0
-    tc._check_conditions(pkt_tcp_22, conditions_any_ssh) == 1
-    tc._check_conditions(pkt_tcp_22, conditions_any_openflow) == 0
+    #*** Call _check_conditions with a packet and a conditions stanza and
+    #***  validate the result is expected dictionary:
+    assert tc._check_conditions(pkt_arp, conditions_any_openflow) == \
+                             results_dict_no_match
+    assert tc._check_conditions(pkt_arp, conditions_all_openflow) == \
+                             results_dict_no_match
+    assert tc._check_conditions(pkt_arp, conditions_any_mac) == \
+                             results_dict_match
+    assert tc._check_conditions(pkt_arp, conditions_all_mac) == \
+                             results_dict_no_match
+    assert tc._check_conditions(pkt_arp, conditions_nested) == \
+                             results_dict_no_match
+    assert tc._check_conditions(pkt_tcp_22, conditions_any_ssh) == \
+                             results_dict_match
+    assert tc._check_conditions(pkt_tcp_22, conditions_any_openflow) == \
+                             results_dict_no_match
 
 #=========== Misc Functions to Generate Data for Unit Tests ===================
-
 def build_packet_ARP():
     """
     Build an ARP packet for use in tests.
@@ -72,7 +83,8 @@ def build_packet_tcp_22():
     # identification=58864,offset=0,option=None,proto=6,src='192.168.56.12',
     # tos=0,total_length=60,ttl=64,version=4)
     #tcp(ack=0,bits=2,csum=60347,dst_port=22,offset=10,
-    # option='\x02\x04\x05\xb4\x04\x02\x08\n\x00\x08\x16\x0f\x00\x00\x00\x00\x01\x03\x03\x06'
+    # option='\x02\x04\x05\xb4\x04\x02\x08\n\x00\x08\x16\x0f\x00\x00\x00\x00\
+    # x01\x03\x03\x06'
     # ,seq=533918719,src_port=52656,urgent=0,window_size=29200)
 
     e = ethernet.ethernet(dst='00:00:00:00:00:02',
@@ -81,8 +93,8 @@ def build_packet_tcp_22():
     i = ipv4.ipv4(version=4, header_length=5, tos=0, total_length=0, 
                     identification=0, flags=0, offset=0, ttl=255, proto=0, 
                     csum=0, src='10.0.0.1', dst='10.0.0.2', option=None)
-    t = tcp.tcp(src_port=1, dst_port=1, seq=0, ack=0, offset=0, bits=0, 
-                      window_size=0, csum=0, urgent=0, option=None)
+    t = tcp.tcp(src_port=52656, dst_port=22, seq=533918719, ack=0, offset=10, 
+                      bits=2, window_size=29200, csum=0, urgent=0, option=None)
     p = packet.Packet()
     p.add_protocol(e)
     p.add_protocol(i)

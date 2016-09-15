76b40cb8df47dd05576928db92eccad3cddb16e4
mattjhayes <matthew_john_hayes@hotmail.com>
2016-09-15 20:21:42 +1200

    Updated setup.py path to see if it's causing RTD issue

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../nmeta'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 
 # -- General configuration ------------------------------------------------

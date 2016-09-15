1bb7d142842fe768683e10ce952b4d4370961d1e
mattjhayes <matthew_john_hayes@hotmail.com>
2016-09-15 20:42:30 +1200

    Commented out theme in setup.py to stop RTD doing Alabaster. Also reinstated path to working one

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../../nmeta'))
 
 
 # -- General configuration ------------------------------------------------
@@ -122,7 +122,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the

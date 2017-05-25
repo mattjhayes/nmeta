b119ad58070af056260f1288153d2596ab8612b4
mattjhayes <matthew_john_hayes@hotmail.com>
2017-05-25 20:50:30 +1200

    Removed non-PIP dependancies from setup.py as causing RTD build to fail

--- setup.py
+++ setup.py
@@ -73,9 +73,8 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['ryu', 'dpkt', 'python-pytest', 'python-yaml',
-                        'simplejson', 'eve', 'coloredlogs', 'voluptuous',
-                        'pymongo'],
+    install_requires=['ryu', 'dpkt', 'simplejson', 'eve', 'coloredlogs',
+                        'voluptuous', 'pymongo'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,

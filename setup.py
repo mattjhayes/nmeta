a392f492c1a16f213a3245ccd9252f49f4854caa
mattjhayes <matthew_john_hayes@hotmail.com>
2017-05-25 21:03:36 +1200

    Trying to change alphabetical to bysource for docstrings export by Sphinx to RTD

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 """
 A setuptools based setup module.
-See:
-https://packaging.python.org/en/latest/distributing.html
-https://github.com/pypa/sampleproject
 """
 
 # Always prefer setuptools over distutils

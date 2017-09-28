1b2ce1654249349037642ea4c406bd30623e4701
Matt <matthew_john_hayes@hotmail.com>
2017-09-28 21:07:43 +1300

    Added colours and TopN to chart

--- nmeta/webUI/js/views/flows_removed_bytes_sent_chart_view.js
+++ nmeta/webUI/js/views/flows_removed_bytes_sent_chart_view.js
@@ -2,6 +2,7 @@ nmeta.FlowsRemovedBytesSentChartView = Backbone.View.extend({
 
     initialize:function () {
         this.model.on("sync", this.render, this);
+        this.default_colors = ['#3366CC','#DC3912','#FF9900','#109618','#990099','#3B3EAC','#0099C6','#DD4477','#66AA00','#B82E2E','#316395','#994499','#22AA99','#AAAA11','#6633CC','#E67300','#8B0707','#329262','#5574A6','#3B3EAC']
     },
 
     // Render ChartJS Chart:
@@ -16,20 +17,22 @@ nmeta.FlowsRemovedBytesSentChartView = Backbone.View.extend({
                     {
                     label: "Flows Removed Bytes Sent",
                     // Use data from model:
-                    data: this.model.flows_removed_stats_bytes_sent_data
+                    data: this.model.flows_removed_stats_bytes_sent_data,
+                    // Fill colours to use in chart:
+                    backgroundColor: this.default_colors
                 }
             ]
         };
         var options = {
             title:{
                 display:true,
-                text:"Flows Removed Bytes Sent"
+                text:"Top Flows Removed by Bytes Sent"
             },
         };
         var ctx = $('#FlowsRemovedBytesSentChart', this.el)[0].getContext("2d");
         
         var FlowsRemovedBytesSentChart = new Chart(ctx, {
-            type: 'pie',
+            type: 'doughnut',
             data: data,
             options: options
         });

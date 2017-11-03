fe03ed9536d29613978c5286a6b98e5769cf99e7
Matt <matthew_john_hayes@hotmail.com>
2017-11-03 22:14:31 +1300

    working on bug fix

--- nmeta/custom_classifiers/ml_training_data_collector_1.py
+++ nmeta/custom_classifiers/ml_training_data_collector_1.py
@@ -70,10 +70,10 @@ class Classifier(object):
             result = 'ML'
             #*** Identity of destination to help with ground truth marking:
             identity_record = ident.get_service_by_ip(flow.packet.ip_dst)
-            if identity_record != 0:
-                service_name = identity_record['service_name']
-            else:
+            if identity_record == 0:
                 service_name = ""
+            else:
+                service_name = identity_record['service_name']
             result += separator + str(service_name)
             #*** Packet header information:
             result += separator + str(flow.packet.ip_src)
